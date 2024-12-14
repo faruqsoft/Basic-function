a6e02b0689c860af9ea4d7bf7251581220ba5df8
faruqsoft <omarfaruqngdc@gmail.com>
2024-12-14 23:19:02 +0600

    Initial commit

---
+++ function.js
@@ -0,0 +1,82 @@
+
+//function definition 72
+//ease understant by w3s
+
+
+//function parameter 73
+/*
+function a (x, y){//(x, y) parameter
+    return x * y
+}
+    
+
+a(3,5)//argument
+
+*/
+
+
+/*
+function a (x, y){//(x, y) parameter
+    return x * y
+}
+    
+
+a(sumit,5)//je kono typer parameter valu deya jay,no type checking
+
+*/
+//argument
+
+// function a (x, y){
+
+//         console.log(arguments);
+//     return x * y
+// }
+    
+
+// a(2,3,4,5,5)
+
+/*
+function a(x, y){
+    x = x * 5;
+    return x * y;
+}
+let m=5;
+let n= 6;
+
+console.log(a(m,n))
+console.log(m)//m change hoy ni
+
+*/
+
+//object passed by refference
+
+/*
+function a(x, y){
+   x.one = 7
+    return x.one * x.two;
+}
+let m = { //object change hoy
+    one: 4,
+    tmo:5,
+}
+
+console.log(a(m));
+console.log(m);
+
+
+*/
+
+
+
+//function invocation 74
+//global object found in window/browser
+//ease understand by w3s
+
+// const x ={
+//     a : function(){
+//         console.log(this)
+//     }
+// }
+// x.a()
+
+
